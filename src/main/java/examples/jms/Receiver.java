f24cee2b2c7ff6ff71abd3371e267618e2d18fd7
Takumi Endo <tq-ed@v7.com>
2014-09-26 00:54:46 +0900

    update jms operation.

---
+++ src/main/java/examples/jms/Receiver.java
@@ -0,0 +1,27 @@
+package examples.jms;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
+
+/**
+ * 
+ * @author t_endo
+ */
+@Stateless
+public class Receiver {
+
+    @Inject
+    private JMSContext context;
+
+    @Resource(mappedName = JMSService.QUEUE)
+    private Queue queue;
+
+    public String startReceiver() {
+        String message = context.createConsumer(queue)
+                .receiveBody(String.class);
+        return message;
+    }
+}
