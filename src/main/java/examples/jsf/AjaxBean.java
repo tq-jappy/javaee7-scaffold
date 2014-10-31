397c4aeb11b7cf71d38cf21572c7c91f6372f05c
Takumi Endo <tq-ed@v7.com>
2014-10-31 20:59:35 +0900

    Change select items according to another select box event.

--- src/main/java/examples/jsf/AjaxBean.java
+++ src/main/java/examples/jsf/AjaxBean.java
@@ -1,7 +1,13 @@
 package examples.jsf;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -28,6 +34,50 @@ public class AjaxBean implements Serializable {
 
     @Getter
     private String b = "b0";
+
+    @Getter
+    @Setter
+    private String c;
+
+    @Getter
+    @Setter
+    private String d;
+
+    @Getter
+    private List<SelectItem> select1;
+
+    @Getter
+    private List<SelectItem> select2;
+
+    @PostConstruct
+    public void init() {
+        select1 = new ArrayList<>();
+        select1.add(new SelectItem("A", "A"));
+        select1.add(new SelectItem("B", "B"));
+
+        select2 = new ArrayList<>();
+    }
+
+    // public void onChange(ValueChangeEvent event) {
+    // c = event.getNewValue().toString();
+    public void onChange() {
+        if (c == null) {
+            select2 = Arrays.asList(new SelectItem("NULL", "NULL"));
+            return;
+        }
+
+        switch (c) {
+        case "A":
+            select2 = Arrays.asList(new SelectItem("X", "X"));
+            break;
+        case "B":
+            select2 = Arrays.asList(new SelectItem("Y", "Y"));
+            break;
+        default:
+            select2 = Arrays.asList(new SelectItem("Z", "Z"));
+            break;
+        }
+    }
 
     public void ajaxA() {
         a = text;
