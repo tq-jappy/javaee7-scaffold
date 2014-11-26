9e93e03f436d4650043c615b399ace87b411224b
Takumi Endo <tq-ed@v7.com>
2014-11-26 23:28:23 +0900

    inject request parameter by CDI

---
+++ src/main/java/examples/cdi/RequestParameter.java
@@ -0,0 +1,17 @@
+package examples.cdi;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD, FIELD, PARAMETER })
+public @interface RequestParameter {
+
+    public String name() default "";
+}
---
+++ src/main/java/examples/cdi/RequestParameterProducer.java
@@ -0,0 +1,52 @@
+package examples.cdi;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+
+/**
+ * CDIでロガーを取得するためのプロデューサ
+ * 
+ * @author t_endo
+ */
+@Named
+@Dependent
+public class RequestParameterProducer implements Serializable {
+
+    private static final long serialVersionUID = 1424981643941504251L;
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private FacesContext facesContext;
+
+    @Produces
+    @RequestParameter
+    public Integer getRequestParameter(InjectionPoint ip) {
+        RequestParameter annotation = ip.getAnnotated().getAnnotation(
+                RequestParameter.class);
+        if (annotation == null) {
+            logger.warn("annotation not given.");
+            return null;
+        }
+
+        String name = annotation.name();
+        if ("".equals(name)) {
+            name = ip.getMember().getName();
+        }
+
+        String value = facesContext.getExternalContext()
+                .getRequestParameterMap().get(name);
+
+        logger.debug("produce RequestParameter ({} = {})", name, value);
+        return (value == null) ? null : Integer.valueOf(value);
+    }
+}
